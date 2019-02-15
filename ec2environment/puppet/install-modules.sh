9e4cf6a6c6839fb7efb083ec8c1f57fd953c37f3
R.I.Pienaar <rip@devco.net>
2019-02-15 15:02:14 +0100

    (#19) fix installation of modules and json ddl files

--- ec2environment/puppet/install-modules.sh
+++ ec2environment/puppet/install-modules.sh
@@ -7,7 +7,5 @@ puppet module install puppetlabs-apache
 puppet module install puppetlabs-ntp
 puppet module install puppetlabs-puppetdb
 puppet module install puppetlabs-puppet_authorization
-puppet module install camptocamp-puppetserver
-puppet module install camptocamp-puppetserver --ignore-dependencies
 puppet module install saz-limits
-
+puppet module install camptocamp-puppetserver --ignore-dependencies
