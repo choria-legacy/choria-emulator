45aabd327ef6b3912dc14c136c3ec66c1493d619
R.I.Pienaar <rip@devco.net>
2019-11-28 18:51:53 +0100

    ensure choria can open many files

--- ec2environment/puppet/modules/profiles/manifests/common.pp
+++ ec2environment/puppet/modules/profiles/manifests/common.pp
@@ -1,4 +1,19 @@
 class profiles::common {
+  file{
+    "/etc/systemd/system/choria-server.service.d":
+      ensure => directory,
+      owner => root;
+
+    "/etc/systemd/system/choria-server.service.d/override.conf":
+      notify => [Exec["systemctl-reload"], Class["choria::service"]],
+      content => "[Service]\nLimitNOFILE=infinity";
+  }
+
+  exec{"systemctl daemon-reload":
+    refreshonly => true,
+    before => Class["choria::service"]
+  }
+
   include mcollective
   include limits
 
