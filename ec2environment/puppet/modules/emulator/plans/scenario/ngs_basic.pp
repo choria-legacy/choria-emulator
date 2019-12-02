90784547daf4837d2db944bc090c8e17111e544c
R.I.Pienaar <rip@devco.net>
2019-12-02 15:39:44 +0100

    support credentials in ngs basic scenario

--- ec2environment/puppet/modules/emulator/plans/scenario/ngs_basic.pp
+++ ec2environment/puppet/modules/emulator/plans/scenario/ngs_basic.pp
@@ -16,13 +16,17 @@
 #  | Choria Servers |                  | Choria Servers |                      | Choria Servers |
 #  +----------------+                  +----------------+                      +----------------+
 plan emulator::scenario::ngs_basic (
+  Optional[String] $credentials = undef,
   Integer $instances = 10,
 ) {
+  $_credentials = emulator::data("emulator_credentials", $credentials)
+
   choria::run_playbook("emulator::scenario::stop_all", {})
 
   info("Starting ${instances} Choria Servers in each region")
   choria::run_playbook("emulator::choria::regional_start", {
     "servers" => "connect.ngs.global:4222",
-    "instances" => $instances
+    "instances" => $instances,
+    "credentials" => $_credentials
   })
 }
